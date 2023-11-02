af4dc9aa9db186f95cbea9f571c1a956464f66c8
Hugo Sibony <hugo.sibony@epita.fr>
2023-11-02 14:30:58 +0100

    part2

--- 2021/day1/part1.c
+++ 2021/day1/part1.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*!
- * Copy the line without the \n at the end
- *
- * param line Current line of the input
- */
-void Strip(char *line)
-{
-    int i = 0;
-    while (line[i] != '\n')
-    {
-        i++;
-    }
-    line[i] = '\0';
-}
-
 int main(void)
 {
     FILE *f = fopen("input", "r");
@@ -25,7 +10,6 @@ int main(void)
     int count = -1; // Because the first Value is not supposed to be counted
     while (fgets(line, sizeof(line), f))
     {
-        Strip(line);
         int currValue = atoi(line);
         if (currValue > previousValue)
             count++;
--- 2021/day1/part2.c
+++ 2021/day1/part2.c
@@ -1,54 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*!
- * Copy the line without the \n at the end
- *
- * param line Current line of the input
- */
-void Strip(char *line)
-{
-    int i = 0;
-    while (line[i] != '\n')
-    {
-        i++;
-    }
-    line[i] = '\0';
-}
-
 int main(void)
 {
     FILE *f = fopen("input", "r");
 
     char line[300];
-    int previousValue = 0;
-    int previousValue1 = 0;
-    int previousValue2 = 0;
-    int count = -1; // Because the first Value is not supposed to be counted
+    int v1 = 0;
+    int v2 = 0;
+    int v3 = 0;
+    int count = 0;
     int mod = 0;
-    while (fgets(line, sizeof(line), f) != NULL)
+
+    //Initialize Data
+    while (mod < 3)
     {
-        Strip(line);
-        int currValue = atoi(line);
-        if (mod > 3) {
-            if (currValue + previousValue + previousValue1 + previousValue2 > previousValue + previousValue1 + previousValue2 ) 
-        }
-        switch (mod % 3)
+        fgets(line, sizeof(line), f);
+
+        switch (mod)
         {
         case 0:
-            previousValue = currValue;
+            v1 = atoi(line);
             break;
         case 1:
-            previousValue1 = currValue;
+            v2 = atoi(line);
             break;
+
         case 2:
-            previousValue2 = currValue;
+            v3 = atoi(line);
             break;
         }
-
 
         mod++;
     }
+
+    //Calculate for the rest of the input
+    while (fgets(line, sizeof(line), f) != NULL)
+    {
+        int current = atoi(line);
+
+        int sum1 = v1 + v2 + v3;
+        int sum2 = v2 + v3 + current;
+
+        if (sum2 > sum1)
+            count++;
+
+        v1 = v2;
+        v2 = v3;
+        v3 = current;
+
+        mod++;
+    }
+
     printf("%d\n", count);
 
     fclose(f);
